3da59edfcffa894b382165f130078e6635404103
yeleshwarapu <69134328+yeleshwarapu@users.noreply.github.com>
2025-06-17 13:31:47 -0500

    first commit

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f7xx_hal.h"
+
+// function declarations (CubeMX usually generates these)
+void SystemClock_Config(void);
+void MX_GPIO_Init(void);
+void MX_CAN1_Init(void);
+
+#endif
